cf8221cd02ada06b9c7507a503d928559e6f8297
Yoeri Nijs <yoeri@posteo.net>
2021-10-10 19:43:58 +0200

    Unneeded

--- src/core/application/v-application.ts
+++ src/core/application/v-application.ts
@@ -35,7 +35,7 @@ export function VApplication(config: VApplicationConfig) {
                 });
 
                 const router = new VRouter(config.routeNotFoundStrategy);
-                this._routes.forEach((route) => router.addRoute(route));
+                this._routes.forEach(route => router.addRoute(route));
             }
         }
 
