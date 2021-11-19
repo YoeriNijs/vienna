ab2eb7b1fd677312e3eede700d5541f25308c703
Yoeri Nijs <yoeri@posteo.net>
2021-11-19 16:09:55 +0100

    fix(release.sh): working on new release script, not tested yet

--- scripts/release.sh
+++ scripts/release.sh
@@ -30,6 +30,9 @@ NEW_FRAMEWORK_VERSION=$(cat package.json \
   | tr -d '[[:space:]]')
 git tag -a "$NEW_FRAMEWORK_VERSION" -m "Release $NEW_FRAMEWORK_VERSION"
 
+git add package.json
+git commit -m "feat(package.json): release $NEW_FRAMEWORK_VERSION"
+
 # Update changelog and push all changes
 npm run version
 
