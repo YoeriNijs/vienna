647e64c3827fc640d2276d5a8e204a4accf2489a
Yoeri Nijs <yoerinijs@gmail.com>
2022-04-18 14:59:37 +0200

    Cookie support (#38)
    * feat(audit): add isuseragentbot audit method
    
    * fix(audit): lint fixes
    
    * feat(vweb): add slugify
    
    * feat(vweb): cookie support
    
    * feat(vweb): add cookie support

--- src/core/misc/web/v-web.ts
+++ src/core/misc/web/v-web.ts
@@ -1,9 +1,12 @@
 import {VInjectable} from "../../injector/v-injectable-decorator";
+import {CookieAttributes, get as getCookieValue, remove as removeCookieValue, set as setCookieValue} from "js-cookie";
 
 export interface VWebOptions {
     trim?: boolean;
     toLowerCase?: boolean;
 }
+
+export type VCookieOptions = CookieAttributes;
 
 @VInjectable({ singleton: false })
 export class VWeb {
@@ -28,5 +31,33 @@ export class VWeb {
             .replace(/[\u0300-\u036f]/g, '')
             .replace(/[^a-zA-Z0-9 ]/g, '')
             .replace(/\s+/g, '-');
+    }
+
+    /**
+     * Returns a cookie for a given name. Will return undefined when the cookie
+     * does not exist.
+     * @param name
+     */
+    getCookie(name: string): string | undefined {
+        return getCookieValue(name);
+    }
+
+    /**
+     * Store a cookie value for a given name. With options, you can specify some cookie
+     * details, such as secure and http only flags.
+     * @param name
+     * @param value
+     * @param options
+     */
+    setCookie(name: string, value: string, options: VCookieOptions = {}): void {
+        setCookieValue(name, value, options);
+    }
+
+    /**
+     * Remove a cookie value for a given name.
+     * @param name
+     */
+    removeCookie(name: string): void {
+        removeCookieValue(name);
     }
 }
