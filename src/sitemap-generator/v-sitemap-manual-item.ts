f56723110ce77fd7f77d8e429df0b93efcd92577
Yoeri Nijs <yoerinijs@gmail.com>
2023-06-09 14:44:53 +0200

    Revert "feat(sitemap-generator): implement a first basic sitemap generator (beta) (#63)" (#64)
    This reverts commit 20b89be3b5dcee44ea26078494be2484fd715f94.

--- src/sitemap-generator/v-sitemap-manual-item.ts
+++
@@ -1,7 +0,0 @@
-import {VSitemapChangefreq} from "./v-sitemap-changefreq";
-
-export interface VSitemapManualItem {
-    location: string;
-    changefreq?: VSitemapChangefreq;
-    priority?: number;
-}
