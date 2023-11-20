2448b536ee7eb4ba370ae478c566f191bcb3c1e4
Yoeri Nijs <yoerinijs@gmail.com>
2023-11-20 22:04:29 +0100

    Update style-inner.component.ts

--- demo/components/style-inner.component.ts
+++ demo/components/style-inner.component.ts
@@ -1,5 +1,9 @@
 import {VComponent} from "../../src";
 
+/**
+ * This demo app is for <b>development purposes only</b>, needed to test some edge cases.
+ * If you want to see a 'real world' example of Vienna, please check https://github.com/YoeriNijs/vienna-demo-app.
+ */
 @VComponent({
     selector: 'style-inner-component',
     style: `div { background-color: red; color: white; padding: 10px; }`,
